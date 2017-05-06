aec0e8d75d0eb9c9c513d1c8aa3e5688b37b28ed
Takuya Saito <t-saito@mesena-net.com>
2017-05-06 22:08:14 +0900

    top/indexアクションの編集 コメント

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,6 @@ class TopController < ApplicationController
   def index
     @question = Question.new
     @questions = current_user.group.questions
+    # @questions = Question.includes(:user).where(users: {group_id: current_user.group_id}).order("questions.created_at DESC")
   end
 end
