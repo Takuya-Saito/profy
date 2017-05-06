a90a418dba461d66c12daf7d6da916c0b6761e1a
Takuya Saito <t-saito@mesena-net.com>
2017-05-06 13:50:49 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 #  root to: 'top#index'
   root controller: :top, action: :index
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
