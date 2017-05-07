c42e7a3a973f35fbb4279602b044b5cddc4dfc8f
Takuya Saito <t-saito@mesena-net.com>
2017-05-07 12:36:02 +0900

    new画面への遷移方法オリジナル1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 #  root to: 'top#index'
+  # get 'questions/:id/answers/new' => 'answers#new'
   root controller: :top, action: :index
   resources :users, only: [:show, :edit, :update]
   resources :groups, only: [:show, :edit, :update]
-  resources :questions, only: [:create]
-  resources :answers, only: [:new, :create]
+  resources :questions, only: [:create] do
+    resources :answers, only: [:new, :create]
+  end
 end
