f5d7d94f5d54ec4398d89c157d61172f54371e42
Takuya Saito <t-saito@mesena-net.com>
2017-05-09 21:58:36 +0900

    top/indexアクションの編集 feed_contents

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,9 @@
 class TopController < ApplicationController
   def index
     @question = Question.new
-    @questions = current_user.group.questions
+    # @questions = current_user.group.questions
     # @questions = Question.includes(:user).where(users: {group_id: current_user.group_id}).order("questions.created_at DESC")
+    feed_contents = current_user.group.feed_contents  #現在ログインしているユーザーのfeed_contentを複数取得する
+    @feed_contents = feed_contents.map(&:content)  #feed_contentsからそれぞれcontentのみ取得する
   end
 end
