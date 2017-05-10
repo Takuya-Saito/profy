85bcd50121e7d0825e1eea059f6383f0a5daa950
Takuya Saito <t-saito@mesena-net.com>
2017-05-10 13:45:04 +0900

    top/indexアクションの編集 paginationの追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,11 @@ class TopController < ApplicationController
     @question = Question.new
     # @questions = current_user.group.questions
     # @questions = Question.includes(:user).where(users: {group_id: current_user.group_id}).order("questions.created_at DESC")
-    feed_contents = current_user.group.feed_contents  #現在ログインしているユーザーのfeed_contentを複数取得する
-    @feed_contents = feed_contents.map(&:content)  #feed_contentsからそれぞれcontentのみ取得する
+    #現在ログインしているユーザーのfeed_contentを複数取得する
+    # feed_contents = current_user.group.feed_contents
+    #feed_contentsからそれぞれcontentのみ取得する
+    # @feed_contents = feed_contents.map(&:content)
+    @feed_contents = current_user.group.feed_contents.includes(:content).page(params[:page]).per(5)
+    @feed_contents_resource = @feed_contents.map(&:content)
   end
 end
